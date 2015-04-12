8cf35f56c204d5385840d5728ab3d59abbd0bb27
Владимир <svr93@i.ua>
2015-04-12 04:53:04 +0400

    get.js___1.0

--- applications/main/app/api/info.json/get.js
+++ applications/main/app/api/info.json/get.js
@@ -1,69 +1,46 @@
 module.exports = function(client, callback) {
-  /* to do:
-    1) to exclude the loop (need transfer (in single func) a whole array
-       instead of elem);
-  */
-
   'use strict';
 
-  var mathLibJS = require(process.cwd() +
-    '/applications/main/app/api/info.json/additional_math_funcs.js');
-    
-  var mathLibCPP = require(process.cwd() +
-    '/applications/main/app/api/info.json/build/Release/addon');
+  var http = require('http');
+  var zlib = require('zlib');
 
-  if (!('latitude' in client.query) || !('longitude' in client.query)) {
+  var STATION_LATITUDE = 60;
+  var STATION_LONGITUDE = 70;
+
+  var query = '?latitude=' + STATION_LATITUDE +
+              '&longitude=' + STATION_LONGITUDE;
+
+  http.get('192.168.0.127/info' + query, function(res) {
+    var chunks = [];
+
+    res.on('data', function(chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on('end', function() {
+      var buffer = Buffer.concat(chunks);
+
+      client.context.data = process(buffer);
+      callback();
+    });
+
+  }).on('error', function(e) {
+
     client.context.data = {
-      err: 'Ошибка запроса'
+      error: e.message
     };
-    
     callback();
-    return;
-  }
 
-  // uptime() - seconds
-  // 1 real second = 100 model seconds
-
-  var currentTime = impress.sandbox.process.uptime() * 100 / (60 * 60 * 24);
-  // currentTime: twenty-four hours
-
-  pgsqlConnection.connection.query(
-    'SELECT * FROM cstl', function(err, queryResult) {
-
-    if (err) {
-      return console.error('error running query', err);
-    }
-
-    var station = {
-      latitude: +client.query.latitude, // degrees
-      longitude: +client.query.longitude // degrees
-    };
-
-    var objArray = queryResult.rows;
-
-    var response = [];
-
-    for (var i = 0; i < objArray.length; ++i) {
-
-      objArray[i].stleccentricity = mathLibJS.getEccentricity(
-        objArray[i].stlperigeeh, objArray[i].stlapogeeh);
-
-      objArray[i].stlsemimajoraxis = mathLibJS.getSemiMajorAxis(
-        objArray[i].stlperigeeh, objArray[i].stleccentricity); // return: km
-
-      var acc = mathLibCPP.calculateAbsCartesianCoords(
-        objArray[i], currentTime);
-
-      var rcc = mathLibCPP.translateAbsCartesianToRelCartesian(
-        acc, currentTime);
-
-      var result = mathLibCPP.calculateStationCoords(rcc, station);
-
-      response.push(result);
-    }
-
-    client.context.data = response;
-    callback();
-    
   });
+
+  function process(buffer) {
+    zlib.gunzip(buffer, function(err, decoded) {
+
+      var coords = JSON.parse(decoded.toString());
+      // need processing
+      return coords;
+
+    });
+  }
+  
 };
