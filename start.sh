952f5b78ea12984cb81918fb5bf712d362d26b5d
Владимир <svr93@i.ua>
2015-04-12 04:21:48 +0400

    exclude_operations_from_start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 iptables -I INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
-mainPath=$(pwd)
-cd applications/main/app/api/info.json
-. build.sh
-cd $mainPath
-psql -U postgres -f fill_db.sql
 node server.js
